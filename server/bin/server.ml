6bce9bb5f5dd8d830441ffa79d05e74c738c2e44
Lucas Viana <lucasviana0604@gmail.com>
2023-09-28 23:27:04 -0300

    Hello world with Opium

--- server/bin/server.ml
+++ server/bin/server.ml
@@ -1,18 +1,15 @@
-open Lwt
-open Cohttp
-open Cohttp_lwt_unix
+open Opium
 
-let server =
-    let callback _conn req body =
-        let uri = req |> Request.uri |> Uri.to_string in
-        let meth = req |> Request.meth |> Code.string_of_method in
-        let headers = req |> Request.headers |> Header.to_string in
-        ( body |> Cohttp_lwt.Body.to_string >|= fun body ->
-          Printf.sprintf "Uri: %s\nMethod: %s\nHeaders\n Headers: %s\nBody: %s" uri
-            meth headers body )
-        >>= fun body -> Server.respond_string ~status:`OK ~body ()
-    in
-    Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback())
+let hello _req = Lwt.return (Response.of_plain_text "Hello")
+let greet req =
+    let name = Router.param req "name" in
+    Printf.sprintf "Hello %s" name
+    |> Response.of_plain_text
+    |> Lwt.return
 
-let () = ignore (Lwt_main.run server)
-
+let app = App.empty
+let () =
+    App.port 3000 app
+    |> App.get "/" hello
+    |> App.get "/greet/:name" greet
+    |> App.run_command
